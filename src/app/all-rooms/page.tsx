ea550edb6c9487dfc1dc30621e5912a6b6a1b162
Princess Glow OS <tatiyanae123@gmail.com>
2026-08-19 22:12:15 -0400

    Reconstruct All Rooms for Batch 10 reference

--- src/app/all-rooms/page.tsx
+++ src/app/all-rooms/page.tsx
@@ -1,4 +1,4 @@
 import { AppShell } from '@/components/app-shell';
-import { Batch7AllRoomsView } from '@/components/batch7/home-world-reference';
+import { Batch10AllRoomsView } from '@/components/batch10/special-features-reference';
 export const dynamic='force-dynamic';
-export default function AllRoomsPage(){return <AppShell><Batch7AllRoomsView/></AppShell>}
+export default function AllRoomsPage(){return <AppShell><Batch10AllRoomsView/></AppShell>}
