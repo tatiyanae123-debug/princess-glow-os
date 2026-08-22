beed4822ce7c59bca31dbfd55f165548a47b0618
Princess Glow OS <tatiyanae123@gmail.com>
2026-08-22 16:32:23 -0400

    Surface new Glow corpus rooms in All Rooms

--- src/app/all-rooms/page.tsx
+++ src/app/all-rooms/page.tsx
@@ -1,4 +1,5 @@
 import { AppShell } from '@/components/app-shell';
 import { Batch10AllRoomsView } from '@/components/batch10/special-features-reference';
+import { CorpusRoomDirectory } from '@/components/knowledge/corpus-room-directory';
 export const dynamic='force-dynamic';
-export default function AllRoomsPage(){return <AppShell><Batch10AllRoomsView/></AppShell>}
+export default function AllRoomsPage(){return <AppShell><div className="space-y-6"><Batch10AllRoomsView/><CorpusRoomDirectory/></div></AppShell>}
