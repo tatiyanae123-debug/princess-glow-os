7e254ca61be4cc5215ea6368326bd504412153b1
Princess Glow OS <tatiyanae123@gmail.com>
2026-09-04 23:31:04 -0400

    Require signed-in user data on Glow OS Home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,10 +5,8 @@ import { GlowCurrentHomeWorld } from '@/components/home/glow-current-home-world'
 export const dynamic = 'force-dynamic';
 
 export default async function HomePage() {
-  if (process.env.VERCEL_ENV !== 'preview') {
-    const session = await auth();
-    if (!session?.user?.id) redirect('/sign-in');
-  }
+  const session = await auth();
+  if (!session?.user?.id) redirect('/sign-in');
 
   return <GlowCurrentHomeWorld />;
 }
