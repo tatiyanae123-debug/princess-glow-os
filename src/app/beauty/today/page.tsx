4e7d0f9b26b94725a0f61719dbede63e4b31026c
Princess Glow OS <tatiyanae123@gmail.com>
2026-09-06 19:25:06 -0400

    Add dedicated Beauty Today route

---
+++ src/app/beauty/today/page.tsx
@@ -0,0 +1,7 @@
+import { BeautyDepthRoom } from '@/components/beauty/beauty-depth-room';
+
+export const dynamic = 'force-dynamic';
+
+export default function BeautyTodayPage() {
+  return <BeautyDepthRoom system="today" />;
+}
