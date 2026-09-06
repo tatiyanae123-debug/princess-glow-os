a20541ae976d29f6239ccdb1e4f27c1e819403a7
Princess Glow OS <tatiyanae123@gmail.com>
2026-09-06 19:24:29 -0400

    Add dedicated Beauty inventory route

---
+++ src/app/beauty/inventory/page.tsx
@@ -0,0 +1,7 @@
+import { BeautyDepthRoom } from '@/components/beauty/beauty-depth-room';
+
+export const dynamic = 'force-dynamic';
+
+export default function BeautyInventoryPage() {
+  return <BeautyDepthRoom system="inventory" />;
+}
