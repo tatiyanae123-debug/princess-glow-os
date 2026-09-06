fe3729d6646b0c15c7a91285ed846ab8feae0258
Princess Glow OS <tatiyanae123@gmail.com>
2026-09-06 19:24:20 -0400

    Add dedicated Beauty devices route

---
+++ src/app/beauty/devices/page.tsx
@@ -0,0 +1,7 @@
+import { BeautyDepthRoom } from '@/components/beauty/beauty-depth-room';
+
+export const dynamic = 'force-dynamic';
+
+export default function BeautyDevicesPage() {
+  return <BeautyDepthRoom system="devices" />;
+}
