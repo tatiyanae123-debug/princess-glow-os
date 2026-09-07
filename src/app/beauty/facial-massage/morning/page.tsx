220aa2ffae774fccde75b611c0b0100d663b0147
Princess Glow OS <tatiyanae123@gmail.com>
2026-09-07 14:57:55 -0400

    Add Morning Light Gua Sha page

---
+++ src/app/beauty/facial-massage/morning/page.tsx
@@ -0,0 +1,7 @@
+import { GuaShaStudioPage } from '@/components/beauty/gua-sha-studio-page';
+
+export const dynamic = 'force-dynamic';
+
+export default function MorningLightGuaShaPage() {
+  return <GuaShaStudioPage view="morning" />;
+}
