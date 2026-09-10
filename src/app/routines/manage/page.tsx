0c174e72a4999c65b2415ad4f89be409ca13f34a
Princess Glow OS <tatiyanae123@gmail.com>
2026-09-09 20:58:44 -0400

    Use reference Routine World on manage landing

--- src/app/routines/manage/page.tsx
+++ src/app/routines/manage/page.tsx
@@ -1,17 +1,11 @@
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
-import { AppShell } from '@/components/app-shell';
-import { RoutineWorldV3 } from '@/components/routines/routine-world-v3';
+import { RoutineReferenceWorld } from '@/components/routines/routine-reference-world';
 
 export const dynamic = 'force-dynamic';
 
 export default async function RoutineManagementPage() {
   const session = await auth();
   if (!session?.user?.id) redirect('/sign-in');
-
-  return (
-    <AppShell>
-      <RoutineWorldV3 />
-    </AppShell>
-  );
+  return <RoutineReferenceWorld />;
 }
