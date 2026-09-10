f03badd59793e77d055c5e5a6476d1b0de443d18
Princess Glow OS <tatiyanae123@gmail.com>
2026-09-09 20:03:41 -0400

    Add contextual Gua Sha journal route

---
+++ src/app/beauty/gua-sha/journal/page.tsx
@@ -0,0 +1,7 @@
+import { GuaShaStudioPage } from '@/components/beauty/gua-sha-studio-page';
+
+export const dynamic = 'force-dynamic';
+
+export default function GuaShaJournalPage() {
+  return <GuaShaStudioPage view="journal" />;
+}
